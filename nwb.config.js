3a2662e4323fb97ef4521daacb73abc252af9ef2
luckarts <luc.bachelerieart@gmail.com>
2020-04-29 10:54:42 +0200

    deploy storybook

--- nwb.config.js
+++ nwb.config.js
@@ -1,4 +1,3 @@
-const path = require('path');
 module.exports = {
 	type: 'react-component',
 	npm: {
@@ -8,18 +7,6 @@ module.exports = {
 			externals: {
 				react: 'React',
 			},
-		},
-	},
-	webpack: {
-		aliases: {
-			// Enable use of 'img/file.png' paths in JavaScript and
-			// "~images/file.png" paths in stylesheets to require an image from
-			// src/images from anywhere in the the app.
-			img: path.resolve('src/img'),
-			// Enable use of require('src/path/to/module.js') for top-down imports
-			// from anywhere in the app, to promote writing location-independent
-			// code by avoiding ../ directory traversal.
-			src: path.resolve('src'),
 		},
 	},
 };
